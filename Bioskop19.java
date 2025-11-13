c6a4cac6560aa9f4e98c1a159c8efc5755811213
nailaamanda883 <nailaamanda883@gmail.com>
2025-11-13 11:38:55 +0700

    modif 6

--- Bioskop19.java
+++ Bioskop19.java
@@ -19,8 +19,14 @@ public class Bioskop19 {
 
         System.out.println(penonton.length);
 
-        for (int i=0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[1].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (String i : penonton[2]) {
+            System.out.println(i);
         }
     }
 }

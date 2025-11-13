49294740529ea4028a1d079143dd22a124493cde
nailaamanda883 <nailaamanda883@gmail.com>
2025-11-13 11:12:40 +0700

    modif 1

--- Bioskop19.java
+++ Bioskop19.java
@@ -11,7 +11,7 @@ public class Bioskop19 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
-        penonton[3][0] = "hana";
+        penonton[3][0] = "Hana";
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
         System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);

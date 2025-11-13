f1fb0df8c99a5c79da6d0c4caf5ce8716bf8644a
nailaamanda883 <nailaamanda883@gmail.com>
2025-11-13 12:03:37 +0700

    Percobaan 2

---
+++ SIAKAD19.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SIAKAD19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] nilai = new int[4][3];
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            double totalPersiswa = 0;
+
+            for (int j=0;j < nilai[i].length; j++) {
+                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPersiswa += nilai[i][j];
+            }
+
+            System.out.println("Nilai rata-rata: " + totalPersiswa/3);
+        }
+
+        System.out.println("\n ==========================================");
+        System.out.println("Rata-rata Nilai Setiap Mata Kuliah:");
+
+        for (int j = 0; j < 3; j++) {
+            double totalPerMatkul = 0;
+            
+            for (int i=0; i < 4; i++) {
+                totalPerMatkul += nilai[1][j];
+            }
+            System.out.println("Mata Kuliah " + (j+1) + ": " + totalPerMatkul/4);
+        }
+    }
+}
